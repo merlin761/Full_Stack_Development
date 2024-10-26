b1c89318ca7c428de24019d26f8b0ed624503cc1
merlinaaa <71297825+merlin761@users.noreply.github.com>
2024-10-25 20:45:02 -0400

    Add files via upload

---
+++ app_admin/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, TripListingComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Travlr Getaways merlin admin side';
+}
---
+++ app_admin/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { TripAddComponent } from './trip-add/trip-add.component';
+import { TripDataService } from './services/trip-data.service';
+import { TripEditComponent } from './trip-edit/trip-edit.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TripListingComponent,
+    TripCardComponent,
+    TripAddComponent,
+    TripEditComponent,
+    NavbarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    TripDataService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
